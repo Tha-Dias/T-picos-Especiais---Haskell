d9fc23a31952df73938db367d60851882a085721
SabSVL <sabrina.venancio.luiz@gmail.com>
2024-03-24 21:21:46 -0300

    cap 2 pronto

--- Atividade.hs
+++ Atividade.hs
@@ -7,8 +7,8 @@ somar x y = y + x
 
 {- a)[1,11,121,1331,14641,161051,1771561]  -}
 
-listaA :: [Int] 
-listaA = iterate (*11) 1
+listaA :: [Int]
+listaA = iterate (* 11) 1
 
 limitando7 :: [Int]
 limitando7 = take 7 listaA
@@ -16,45 +16,69 @@ limitando7 = take 7 listaA
 {- b) [1,2,3,5,6,7,9,10,11,13,14,15,17,18,19,21,22,23, 25,26,27,29,30,31,33,34,35,37,38,39] -}
 
 listaB :: [Int]
-listaB = [1..39]
+listaB = [1 .. 39]
 
 {-c)["AaBB", "AbBB", "AcBB", "AdBB", "AeBB", "AfBB", "AgBB"] -}
 
 listaC :: [String]
-listaC = ["AA" ++ [letra] ++ "BB" | letra <- ['a'..'g']]
-
+listaC = ["AA" ++ [letra] ++ "BB" | letra <- ['a' .. 'g']]
 
 {-d)[5,8,11,17,20,26,29,32,38,41] -}
 
 listaD :: [Int]
-listaD = [5, 8..41]
+listaD = [5, 8 .. 41]
 
 {-e)[1.0,0.5,0.25,0.125,0.0625,0.03125] -}
 
 listaE :: [Double]
-listaE = take 6 (iterate (/2) 1.0)
+listaE = take 6 (iterate (/ 2) 1.0)
 
 {-f)[1,10,19,28,37,46,55,64] -}
 
 listaF :: [Int]
-listaF = [1,10..64]
+listaF = [1, 10 .. 64]
 
 {-g)[2,4,8,10,12,16,18,22,24,28,30] -}
 
 listaG :: [Int]
-listaG = [x | x <- [2, 4..30], x `notElem` [20]] {- `notElem`retira i=o valor dentro dos colchetes da lista -}
+listaG = [x | x <- [2, 4 .. 30], x `notElem` [20 {- `notElem`retira i=o valor dentro dos colchetes da lista -}]]
 
-{-h)['@','A','C','xxD','E','G','J','L']-} 
+{-h)['@','A','C','xxD','E','G','J','L']-}
 
 listaH :: [String]
 listaH = ["@", "A", "C", "xxD", "E", "G", "J", "L"]
 
 {- ------------------------------------------------------------------ -}
 
+{- 2.1)	 Crie	 uma	 função	 que	 verifique	 se	 o	 tamanho	 de	 uma
+String	é	par	ou	não.	Use		Bool		como	retorno. -}
+
+tamanhoPar :: String -> Bool
+tamanhoPar str = even (length str)
+
+{- ------------------------------------------------------------------ -}
+
+{- 2.3)	 Escreva	 uma	 função	 que	 receba	 um	 vetor	 de	 Strings	 e
+retorne	uma	lista	com	todos	os	elementos	em	ordem	reversa. -}
+
+listaReversa :: [String] -> [String]
+listaReversa = reverse
+
+{- ------------------------------------------------------------------ -}
+
+{- 2.4)	 Escreva	 uma	 função	 que	 receba	 um	 vetor	 de	 Strings	 e
+retorne	 uma	lista	 com	 o	 tamanho	 de	 cada	 String.	As	palavras	 de
+tamanho	par	devem	ser	excluídas	da	resposta. -}
+
+removerPar :: [String] -> [Int]
+removerPar = map length . filter (odd . length)
+
+{- ------------------------------------------------------------------ -}
+
 {- 2.5) Escreva a função head como composição de duas outras.  -}
 
-multiplica11 :: [Integer] 
-multiplica11 = iterate (*11) 1
+multiplica11 :: [Integer]
+multiplica11 = iterate (* 11) 1
 
 limitando :: [Integer]
 limitando = take 7 multiplica11
@@ -69,3 +93,11 @@ pegandoHead = head limitando
 recebeString :: String -> Bool
 recebeString palavra = palavra == reverse palavra
 
+{- -------------------------------------------------------------------- -}
+
+{- 2.7)	 Faça	 uma	 função	 que	 receba	 um	 inteiro	 e	 retorne	 uma
+tupla,	contendo:	o	dobro	deste	número	na	primeira	coordenada,	o
+triplo	na	segunda,	o	quádruplo	na	terceira	e	o	quíntuplo	na	quarta. -}
+
+tuplaN :: Int -> (Int, Int, Int, Int)
+tuplaN x = (x * 2, x * 3, x * 4, x * 5)
