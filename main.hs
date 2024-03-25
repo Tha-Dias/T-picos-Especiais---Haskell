b18776bb789878bddead4e8edf2605ac5489bb19
SabSVL <sabrina.venancio.luiz@gmail.com>
2024-03-24 21:12:23 -0300

    2.4 feita

--- main.hs
+++ main.hs
@@ -14,3 +14,6 @@ listaReversa = reverse
 {- 2.4)	 Escreva	 uma	 função	 que	 receba	 um	 vetor	 de	 Strings	 e
 retorne	 uma	lista	 com	 o	 tamanho	 de	 cada	 String.	As	palavras	 de
 tamanho	par	devem	ser	excluídas	da	resposta. -}
+
+removerPar:: [String] -> [Int]
+removerPar = map length . filter (odd . length)
