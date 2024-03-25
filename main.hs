b4d8bfbe2dd5960e09bdc68aaf0ab17c14163e86
SabSVL <sabrina.venancio.luiz@gmail.com>
2024-03-24 21:19:20 -0300

    2.7 feita

--- main.hs
+++ main.hs
@@ -17,3 +17,12 @@ tamanho	par	devem	ser	excluídas	da	resposta. -}
 
 removerPar:: [String] -> [Int]
 removerPar = map length . filter (odd . length)
+
+{- 2.7)	 Faça	 uma	 função	 que	 receba	 um	 inteiro	 e	 retorne	 uma
+tupla,	contendo:	o	dobro	deste	número	na	primeira	coordenada,	o
+triplo	na	segunda,	o	quádruplo	na	terceira	e	o	quíntuplo	na	quarta. -}
+
+
+tuplaN:: Int -> (Int, Int, Int, Int)
+tuplaN x= (x*2, x*3, x*4, x*5) 
+
